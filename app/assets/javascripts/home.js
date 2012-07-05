013b97e4412ef0f286cef392673842d6f435df39
Pixelephant Media <pixelephant@pixelephant.hu>
2012-07-05 16:38:42 +0200

    no message

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,9 @@
+//= require jquery.appear.min
+
 $(document).ready(function(){
 
-
+$('.animated').appear(function(){
+   		$(this).addClass("bounceIn");
+	});
 
 });
---
+++ app/assets/javascripts/workflow.js
@@ -0,0 +1,23 @@
+//= require jquery.appear.min
+
+$(document).ready(function(){
+
+		$('#testing_enter').click(function() {
+		  $('#testing_rocket').animate({
+		    top: "-60%"
+		  }, 1500,"easeInOutExpo", function() {
+		    $("#testing_rocket").fadeOut("fast",function(){
+		    	$("#testing_rocket").css("top","30%");
+		    	$("#testing_rocket").fadeIn();
+		    })
+		  });
+		});
+
+
+
+	$('.animated').appear(function(){
+   		$(this).addClass("bounceIn");
+	});
+
+
+});
