fdc1d391df533a9b6b9faf7fe2d00e2e35b58ce2
Norbert Pap <norbert.pap@pixelephant.hu>
2012-07-13 14:59:36 +0200

    no message

--- app/assets/javascripts/method.js
+++ app/assets/javascripts/method.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 
 
-	$("#main a").click(function(){
-		var w = $(this).html();
+	$("#try img").click(function(){
+		var w = $(this).data("width");
 		var h = $(window).height();
 		window.open (window.location.href,"mywindow","menubar=1,resizable=1,width="+w+",height="+h+"");
 		return false;
 	})
 
+	$("#test form").submit(function(){
+		var w = $(this).find(":checked").data("width");
+		var h = $(window).height();
+		window.open ($("input[type='url']").val(),"mywindow1","menubar=1,resizable=1,width="+w+",height="+h+"");
+		return false;
+	})
 });
