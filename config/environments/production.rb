e05265e1e4cf2f7d1435ace9ad4740ca85b18bf6
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-07-24 12:41:11 +0200

    production
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,10 @@ Resppix::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+#  config.assets.precompile += %w( about.css, home.css, method.css, portfolio.css, services.css, workflow.css, normalize.css, home.js, jquery.appear.min.js, jquery.easing.js, jquery.fittext.js, method.js, modernizr.js, portfolio.js, services.js, workflow.js)
+
+config.assets.precompile += %w(*.css, *.scss, *.coffee, *.js)
+config.assets.precompile += ['home.css', 'method.css', 'about.css', 'portfolio.css', 'services.css', 'workflow.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
