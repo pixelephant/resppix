b8bb117b6151bddabd2f20f97c819b7bc2708699
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-07-24 12:51:40 +0200

    initializers
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Resppix::Application.config.session_store :cookie_store, key: '_resppix_session'
+Resppix::Application.config.session_store :cookie_store, :key => '_resppix_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
