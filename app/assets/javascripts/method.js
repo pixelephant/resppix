b50018693aa658704948c92a08554cc089c965b8
Norbert Pap <norbert.pap@pixelephant.hu>
2012-08-27 16:09:22 +0200

    method js

--- app/assets/javascripts/method.js
+++ app/assets/javascripts/method.js
@@ -8,8 +8,8 @@ $('.animated').appear(function(){
 	});
 
 
-	$("#try span").click(function(){
-		var w = $(this).find("img").data("width");
+	$("#try img").click(function(){
+		var w = $(this).data("width");
 		var h = $(window).height();
 		window.open (window.location.href,"mywindow","menubar=1,resizable=1,width="+w+",height="+h+"");
 		return false;
