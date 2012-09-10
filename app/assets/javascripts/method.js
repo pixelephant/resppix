69787418df3a9dad8927ad55a5a821682c3594f0
Norbert Pap <norbert.pap@pixelephant.hu>
2012-09-10 14:21:59 +0200

    büg

--- app/assets/javascripts/method.js
+++ app/assets/javascripts/method.js
@@ -11,14 +11,14 @@ $('.animated').appear(function(){
 	$("#try img").click(function(){
 		var w = $(this).data("width");
 		var h = $(window).height();
-		window.open (window.location.href,"mywindow","menubar=1,resizable=1,width="+w+",height="+h+"");
+		window.open (window.location.href,"mywindow","menubar=1,scrollbars=1,resizable=1,width="+w+",height="+h+"");
 		return false;
 	})
 
 	$("#test form").submit(function(){
 		var w = $(this).find(":checked").data("width");
 		var h = $(window).height();
-		window.open ($("input[type='url']").val(),"mywindow1","menubar=1,resizable=1,width="+w+",height="+h+"");
+		window.open ($("input[type='url']").val(),"mywindow1","menubar=1,scrollbars=1,resizable=1,width="+w+",height="+h+"");
 		return false;
 	})
 });
