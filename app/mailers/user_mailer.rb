eb905c3588225801ca8044ac1c38e8a369c44a1b
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-11-14 13:55:49 +0100

    emailekben user agent
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,20 +2,22 @@
 class UserMailer < ActionMailer::Base
   default :from => "balazs.antal@pixelephant.hu"
 
-  def contact_email(name,phone,email,brief)
+  def contact_email(name,phone,email,brief,env)
   	@name = name
   	@phone = phone
   	@email = email
   	@brief = brief
+    @env = env
     mail(:to => "balazs.antal@pixelephant.hu", :subject => "Kontakt")
   end
 
-  def inquery_email(name,phone,email,brief,budget)
+  def inquery_email(name,phone,email,brief,budget,env)
   	@name = name
   	@phone = phone
   	@email = email
   	@brief = brief
   	@budget = budget
+    @env = env
     mail(:to => "balazs.antal@pixelephant.hu", :subject => "Árajánlat")
   end
 
